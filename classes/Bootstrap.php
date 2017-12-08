76780dd3fef0393889121a0494b75d25a1b2bc70
hackpb <hackpb.vlg@gmail.com>
2017-12-08 17:18:32 +0000

    first commit of miniF

---
+++ classes/Bootstrap.php
@@ -0,0 +1,50 @@
+<?php
+class Bootstrap {
+	private $controller;
+	private $action;
+	private $request;
+
+	public function __construct ($request){
+		$this->request = $request;
+		if($this->request['controller'] ==""){
+			$this->controller = 'home';
+		}else{
+			$this->controller = $this->request['controller'];
+		}
+		if($this->request['action'] == ""){
+			$this->action = 'index';
+		}else{
+			$this->action = $this->request['action'];
+		}
+
+		echo $this->controller; //test where you are (controler)
+		echo $this->action;//est where you are (action)
+	}
+
+	public function createController(){
+		//check class
+		if(class_exists($this->controller)){
+			$parents = class_parents($this->controller);
+			//check if is extended
+			if(in_array("controller", $parents)){
+				if(method_exists($this->controller, $this->action)){
+					return new $this->controller($this->action, $this->request);
+				}else{
+					//method not exists
+					echo "<h1>Method not exists</h1>";
+					return;
+				}
+			}else{
+				//base controller not found
+				echo "<h1>base controller not found</h1>";
+				return;
+			}
+		}else{
+			//controller class not found
+			echo "<h1>Controller class not found</h1>";
+			return;
+		}
+	} 
+}
+
+?>
